cb2763d2e4ec2f3cc0d5c4135d81fff38f02f317
CerenAlper <112765777+CerenAlper@users.noreply.github.com>
2022-09-07 14:31:19 +0300

    Update and rename README.md to hw1.php

---
+++ hw1.php
@@ -0,0 +1,16 @@
+    <?php
+    function ucgen($sayi){
+        for($i=0; $i<=$sayi; $i++){
+            $j=0;
+                while($j<=$i){
+                    $j++;
+                    echo $j*0;
+
+                 };
+            echo "<br>";
+
+        };
+    };
+    ucgen(15);
+
+    ?>
